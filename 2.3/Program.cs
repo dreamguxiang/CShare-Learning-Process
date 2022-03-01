50166807e28903a3bba4ffceb21d2c84d7430a37
qingyu <62042544+dreamguxiang@users.noreply.github.com>
2022-03-01 10:03:00 +0800

    2.3

---
+++ 2.3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Program05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2-5
+            int num1 = 0;
+            int num2 = 0;
+            int num3 = 0;
+            bool res1, res2;
+
+            res1 = num1 > 0 & num2++ > 1;
+            Console.WriteLine(res1);
+            Console.WriteLine("num2 = "+num2);
+
+            res2 = num2 > 0 && num3++ > 1;
+            Console.WriteLine(res2);
+            Console.WriteLine("num3 = "+num3);
+
+            Console.ReadKey();
+        }
+    }
+}
